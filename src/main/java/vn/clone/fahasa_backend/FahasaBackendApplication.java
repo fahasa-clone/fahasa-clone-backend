28914ae416985ea64b3bfe4dae549f47bff8fc13
Minh Kop <87182623+Minh-Kop@users.noreply.github.com>
2025-08-25 22:08:42 +0700

    First commit

---
+++ src/main/java/vn/clone/fahasa_backend/FahasaBackendApplication.java
@@ -0,0 +1,13 @@
+package vn.clone.fahasa_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FahasaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FahasaBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/vn/clone/fahasa_backend/FahasaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.clone.fahasa_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FahasaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
